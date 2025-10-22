32abf4cb763cc6e49145329955be4c0b439d3d0c
vinceopaul <82008765+vinceopaul@users.noreply.github.com>
2025-10-22 12:33:39 +0800

    chore(practice): add practice lines to 005 script

--- js/005-logical-test.js
+++ js/005-logical-test.js
@@ -8,9 +8,9 @@ if (!(age1 >= 14 && age1 <= 90)) console.log("not between 14 and 90");
 if (age1 < 14 || age1 > 90) console.log("not between 14 or 90");
 
 //about "if"
-// if (-1 || 0) alert("first"); //-1, first truthy
-// if (-1 && 0) alert("second"); // 0, falsy
-// if (null || (-1 && 1)) alert("third"); // 1, truthy
+if (-1 || 0) alert("first"); //-1, first truthy
+if (-1 && 0) alert("second"); // 0, falsy
+if (null || (-1 && 1)) alert("third"); // 1, truthy
 
 //Check the login
 let userRole = prompt("Who's there?");
@@ -35,4 +35,51 @@ if (userRole === null || userRole.trim() === "") {
       alert("Welcome!");
       break;
   }
+}
+
+// The name of JavaScript
+let javaScriptName = prompt('What is the "official" name of JavaScript?');
+
+if (javaScriptName === "ECMAScript") alert("Right!");
+else alert('You don\'t know? "ECMAScript"!');
+
+// Show the sign
+let num = prompt("Input a digit number");
+console.log(typeof num);
+
+if (num > 0) alert(1);
+else if (num < 0) alert(-1);
+else alert(0);
+
+//Rewrite 'if' into "?"
+let result = a + b < 4 ? "Below" : "Over";
+
+//Rewrite 'if...else' into '?'
+//! Note! Do not do this. Unreadable!
+
+let login = prompt("H");
+let message =
+  login == "Employee"
+    ? "Hello"
+    : login == "Director"
+    ? "Greetings"
+    : login == ""
+    ? "No login"
+    : "";
+alert(message);
+
+//Rewrite "if" into "switch"
+let a = +prompt("a?");
+
+switch (a) {
+  case 0:
+    alert(0);
+    break;
+  case 1:
+    alert(1);
+    break;
+  case 2:
+  case 3:
+    alert("2,3");
+    break;
 }
